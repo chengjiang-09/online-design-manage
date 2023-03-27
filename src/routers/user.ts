3a2f532c440a84e4d557ebf9f17fd8d05d46d27a
unknown <chengjiang_09@163.com>
2023-03-27 18:15:15 +0800

    完善swagger部分注释

--- src/routers/user.ts
+++ src/routers/user.ts
@@ -4,15 +4,101 @@ import LoginController from "../controllers/LoginController";
 
 const userRouter = new KoaRouter();
 
+/**
+ * @swagger
+ * openapi: 3.0.0
+ *
+ * /design/login/email:
+ *   get:
+ *     summary: 邮箱验证码登录
+ *     description: 邮箱验证码登录
+ *     tags:
+ *       - user
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *       - name: email
+ *         in: body
+ *         description: 邮箱
+ *         required: true
+ *         schema:
+ *           type: string
+ *       - name: code
+ *         in: body
+ *         description: 邮箱验证码
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: 返回用户token和对应权限路由
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 token:
+ *                   type: string
+ *                   description: token
+ *                 routes:
+ *                   type: array
+ *                   description: 路由信息
+ *                   items:
+ *                     type: object
+ *
+ */
 userRouter.post("/login/email", LoginController.loginByEmail);
 
+/**
+ * @swagger
+ * openapi: 3.0.0
+ *
+ * /design/login/password:
+ *   get:
+ *     summary: 邮箱验证码登录
+ *     description: 邮箱验证码登录
+ *     tags:
+ *       - user
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *       - name: email
+ *         in: body
+ *         description: 邮箱
+ *         required: true
+ *         schema:
+ *           type: string
+ *       - name: code
+ *         in: body
+ *         description: 密码
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: 返回用户token和对应权限路由
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 token:
+ *                   type: string
+ *                   description: token
+ *                 routes:
+ *                   type: array
+ *                   description: 路由信息
+ *                   items:
+ *                     type: object
+ *
+ */
 userRouter.post("/login/password", LoginController.loginByPassword);
 
 /**
  * @swagger
  * openapi: 3.0.0
  *
- * /login/emailCode:
+ * /design/login/emailCode:
  *   get:
  *     summary: 获取邮件验证码
  *     description: 获取邮件验证码
@@ -28,10 +114,6 @@ userRouter.post("/login/password", LoginController.loginByPassword);
  *     responses:
  *       200:
  *         description: 无具体返回值
- *         content:
- *           application/json:
- *             schema:
- *               type: array
  */
 userRouter.get("/login/emailCode", EmailCodeController.sendCode);
 
